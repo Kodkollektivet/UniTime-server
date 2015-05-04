2d8f32a5e38352a1e2d733f7d0bb9d869cb6eccc
John Herrlin <jherrlin@gmail.com>
2015-05-04 22:17:25 +0200

    Views.py updated with comments

--- server/django/timeedit/views.py
+++ server/django/timeedit/views.py
@@ -60,26 +60,52 @@ class IndexView(generic.View):
 
 class CourseView(generic.View):
 
+    '''
+    This method is for accepting client requests that dont provide a csrf token.
+    '''
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(CourseView, self).dispatch(request, *args, **kwargs)
-        
-    # This GET returns ALL courses WITH info, can be much data here so be aware!
+    
+    '''    
+    This GET returns ALL courses WITH info, can be much data here so be aware!
+    '''
     def get(self, request, *args, **kwargs):
+        
+        # Returns json of all of the Courses that are saved in the database! Be aware of big data!
         return HttpResponse(json.dumps(serializers.serialize('json', Course.objects.all())), content_type='application/json')
 
-    # When POST here, only the specific course info will be sent
+    
+    '''
+    When POST here, only the specific course info will be sen
+    '''
     def post(self, request, *args, **kwargs):
+
+        # Empty form, the reason a form i here is because we do data validation with forms. Check out CourseForm inside of forms.py
         form = CourseForm(request.POST)
-        print(request.POST)
+
+        # If form data is valid, continue. Else send error messages
         if form.is_valid():
+
+            # Get the cleaned data from the form.
             course = form.cleaned_data['course'].upper()
+
+            # Try to get the object from the database
             try:
+
+                # If object is found, serialize it and send it as json
                 return HttpResponse(json.dumps(serializers.serialize('json', [Course.objects.get(course_code=course),])), content_type='application/json')
+
+            # If object not found in database, send this
             except Course.DoesNotExist as e:
+
+                # Send a simple json object with error message
                 return HttpResponse(json.dumps({'message':'Can not find that course'}), content_type='application/json')
+
+        # If form is invalid, send simple json object
         return HttpResponse(json.dumps({'message':'Invalid input'}), content_type='application/json')
-        
+
+    
 class EventView(generic.View):
 
     def post(self, request, *args, **kwargs):
