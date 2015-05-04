0812a9feab8f42aa346f42c5807b91ea06685e65
John Herrlin <jherrlin@gmail.com>
2015-05-04 22:56:24 +0200

    Endpoint for requesting course events created. endpont is at /api/event/

--- server/django/settings/urls.py
+++ server/django/settings/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from timeedit.views import IndexView, allCouseCodesInJSON, allCoursesInJSON, CourseView
+from timeedit.views import IndexView, allCouseCodesInJSON, allCoursesInJSON, CourseView, EventView
 
 from timeedit.api_handler import getAllCourseCodes
 
@@ -13,7 +13,8 @@ urlpatterns = [
     #url(r'^getall/$', getAllCourseCodes, name='getall'),
     
     url(r'^api/only-course-codes/$', allCouseCodesInJSON, name='only-course-codes'),
-    url(r'^api/course/$', CourseView.as_view(), name='courses'),
-
+    url(r'^api/course/$', CourseView.as_view(), name='course'),
+    url(r'^api/event/$', EventView.as_view(), name='event'),
+    
     url(r'^admin/', include(admin.site.urls)),
 ]
--- server/django/timeedit/views.py
+++ server/django/timeedit/views.py
@@ -35,7 +35,7 @@ class IndexView(generic.View):
             try:
                 course = Course.objects.get(course_code=course_post)
                 
-                defaultLogger = logging.getLogger('defaultLogger')                
+                defaultLogger = logging.getLogger('defaultLogger')
                 defaultLogger.info('----------------FETCHED FROM DB---------------')
                 defaultLogger.info('Course: %s' % course_post)
                 defaultLogger.info('-----------------END OF FETCH-----------------')
@@ -107,10 +107,45 @@ class CourseView(generic.View):
 
     
 class EventView(generic.View):
+    
+    '''
+    This method is for accepting client requests that dont provide a csrf token.
+    '''
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(EventView, self).dispatch(request, *args, **kwargs)
 
+    
     def post(self, request, *args, **kwargs):
         pass
+        # Empty form, the reason a form i here is because we do data validation with forms. Check out CourseForm inside of forms.py
+        form = CourseForm(request.POST)
 
+        # If form data is valid, continue. Else send error messages
+        if form.is_valid():
+
+            # Get the cleaned data from the form.
+            course = form.cleaned_data['course'].upper()
+
+            # Try to get the object from the database
+            try:
+                # Course object from database
+                course_object = Course.objects.get(course_code=course)
+                
+                # If object is found, serialize it to the getCourseEvents function
+                # This function calls LNUs timeedit API, this API returns JSON
+                return HttpResponse(json.dumps(getCourseEvents(course_object.season, course_object.year, course_object.course_anmalningskod)), content_type='application/json')
+
+            # If object not found in database, send this
+            except Course.DoesNotExist as e:
+
+                # Send a simple json object with error message
+                return HttpResponse(json.dumps({'message':'Can not find that course'}), content_type='application/json')
+
+        # If form is invalid, send simple json object
+        return HttpResponse(json.dumps({'message':'Invalid input'}), content_type='application/json')
+
+    
 def allCouseCodesInJSON(request):
     return HttpResponse(json.dumps(map(lambda c: c.course_code, Course.objects.all())),content_type='application/json')
 
