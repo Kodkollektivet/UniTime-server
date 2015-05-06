547df4ee35f4cf29065286cec0f172ea77221faf
rasmus <ras.sjostrom@hotmail.com>
2015-05-06 23:43:38 +0200

    Updated log function

--- server/django/timeedit/api/timeedit_handler.py
+++ server/django/timeedit/api/timeedit_handler.py
@@ -38,7 +38,8 @@ def getCourseEvents(season, year, course_anmalningskod):
     try:
         # Url string
         url = '/web/lnu/db1/schema1/s.json?object=courseevt_%s%s-%s&tab=3' % (str(season), str(year), str(course_anmalningskod))
-
+        defaultLogger.info('URL: %s' % url)
+        
         # HTTPS connection
         connection = httplib.HTTPSConnection('se.timeedit.net')
         
@@ -49,26 +50,27 @@ def getCourseEvents(season, year, course_anmalningskod):
             'User-Agent':'lnu-timeedit-app',
             'Vary':'Accept-Charset, Accept-Encoding, Accept-Language, Accept',
         }
-
+        
+        
         # Create the request
         connection.request('GET', url, {}, header)
-
+        
         # Read the json data and create a python dict
         json_data = json.loads(connection.getresponse().read())
         
         # Pretty print it, only use thins when looking for all data
         #pp = pprint.PrettyPrinter(indent=4)
         #pp.pprint(json_data['messages'])
-
+        
         event_list = []
         try:
             json_data['messages']
         except KeyError as e:
             pass
-
+        
         try:
             for i in json_data['reservations']:
-
+                
                 data = {
                     'startdate':'',
                     'starttime':'',
@@ -92,6 +94,8 @@ def getCourseEvents(season, year, course_anmalningskod):
             
     except ValueError as e:
         pass
+
+    
     
     return [{
         'startdate':'',
@@ -106,8 +110,11 @@ def getCourseEvents(season, year, course_anmalningskod):
 # This function returns a list
 def getCourseId(course_code):
     # Try to connect
+    defaultLogger.info('Requesting course id...')
     try:
         req = requests.get('https://se.timeedit.net/web/lnu/db1/schema2/objects.txt?max=15&fr=t&partajax=t&im=f&sid=6&l=en_US&search_text='+course_code+'%20&types=5')
+        defaultLogger.info('REQUEST: %s' % req)
+        
         data = json.loads(req.text)
         try:
             data = data['records']
@@ -122,21 +129,36 @@ def getCourseId(course_code):
                 if (course['fields'][2]['values'][0] == __THIS_SEMESTER + __THIS_YEAR):
                     course_code_list.append(course['textId'])
 
+            defaultLogger.info('Successfully retrieved course IDs!')
+            defaultLogger.info(' ')
+            
             return course_code_list
         
         except KeyError as e:
-            # LOG THIS ERROR
-            print('Error in: '),
-            print(e)
-                
+            defaultLogger.info(e)
+            defaultLogger.info('Retrieving Course IDs failed due to KeyError Exception...')
+            
+            errorLogger.info('KeyError in timeedit_handler.getCourseId:')
+            errorLogger.info(e)
+            errorLogger.info(' ')
+            
     except requests.exceptions.ConnectionError as e:
-        # LOG THIS ERROR
-        print(e)
+        defaultLogger.info(e)
+        defaultLogger.info('Retrieving Course IDs failed due to ConnectionError Exception...')
+
+        errorLogger.info('ConnectionError in timeedit_handler.getCourseId:')
+        errorLogger.info(e)
+        errorLogger.info(' ')
 
         
 def getCourseInfo(course_id):
+    
+    defaultLogger.info('Requesting course information for course_id: %s...' % course_id)
     try:
         req = requests.get('https://se.timeedit.net/web/lnu/db1/schema2/objects/'+course_id+'/o.json')
+
+        defaultLogger.info('REQUEST: %s' % req)
+        
         pp = pprint.PrettyPrinter(indent=4)
         #pp.pprint(req.text)
 
@@ -147,7 +169,9 @@ def getCourseInfo(course_id):
         data = data[0]['fields']
         data = data
         #pp.pprint(data)
-
+        defaultLogger.info('Success!')
+        defaultLogger.info(' ')
+        
         return {
             'name':data[1]['values'][0],           
             'course_code':data[0]['values'][0],
@@ -160,9 +184,13 @@ def getCourseInfo(course_id):
         }
             
 
-    except requests.exceptions.ConnectionError as a:
-        # LOG THIS
-        print(e)
+    except requests.exceptions.ConnectionError as e:
+        defaultLogger.info(e)
+        defaultLogger.info('Retrieving course info failed due to ConnectionError Exception...')
+        
+        errorLogger.info('ConnectionError in timeedit_handler.getCourseInfo:')
+        errorLogger.info(e)
+        errorLogger.info(' ')
 
 
 
--- server/django/timeedit/views.py
+++ server/django/timeedit/views.py
@@ -48,6 +48,8 @@ class IndexView(generic.View):
         form = CourseForm(request.POST)
         
         searchLogger = logging.getLogger('searchLogger')
+        defaultLogger = logging.getLogger('defaultLogger')
+        errorLogger = logging.getLogger('errorLogger')
         ip = get_ip(request)
 
         # If form is valid
@@ -58,66 +60,97 @@ class IndexView(generic.View):
             
             # Logs a valid post before it reaches api_handler
             searchLogger.info('Search Term: %s  IP Addr: %s' % (course_post, ip))
+            defaultLogger.info('Searching for %s...' % course_post)
             
             # Try to get if course from database
             try:
+                defaultLogger.info('Looking for single course object in database...')
+                
                 # If there is only one course for the course code in database
                 course = Course.objects.get(course_code = course_post)
+                course_events = getCourseEvents(course.semester, course.year, course.course_reg)
+                
                 # Logs a fetch from the db
                 defaultLogger = logging.getLogger('defaultLogger')
                 defaultLogger.info('----------------FETCHED FROM DB---------------')
                 defaultLogger.info('Course: %s' % course_post)
+                defaultLogger.info('Single object')
                 defaultLogger.info('-----------------END OF FETCH-----------------')
                 defaultLogger.info(' ')
+                
                 return render(request,
                               'timeedit/index.html',
                               {'course' : course,
-                               'events' : getCourseEvents(course.semester, course.year, course.course_reg),
+                               'events' : course_events,
                                'form' : form}
                 )
-
-            # If there is mulit course objects in the database
+             
+            # If there is multiple course objects in the database
             except MultipleObjectsReturned as e:
+                defaultLogger.info('Looking for multiple course object in database...')
+                
                 courses = Course.objects.filter(course_code = course_post)
                 course_events_list = []
-                
+                 
                 for i in courses:
                     course_events_list.append(getCourseEvents(i.semester, i.year, i.course_reg))
                     
+                # Logs a fetch from the db
+                defaultLogger.info('------------------FETCHED FROM DB-----------------')
+                defaultLogger.info('Course: %s' % course_post)
+                defaultLogger.info('Multiple Objects')
+                defaultLogger.info('-------------------END OF FETCH-------------------')
+                defaultLogger.info(' ')
+                
                 return render(request,
                               'timeedit/index.html',
                               {'course' : courses[0],
                                'events' : max(course_events_list),
                                'form' : form}
                 )
-
                 
                         
-            # If course dont exist in the database
+            # If course dont exist in the database, request it
             except Course.DoesNotExist as e:
-
+                defaultLogger.info('No matching course found in database. Requesting from TimeEdit...')
+                 
                 try:
+                    
+                    defaultLogger.info('----------------INITIATING REQUEST----------------')
+                    
                     courses_list_id = getCourseId(course_post)  # The stupid id thats is assigned to every course
                     events_list = []
                     courses_list = []
-                
+                    
                     for i in courses_list_id:
                         course = Course(**getCourseInfo(i))
                         course.save()
                         courses_list.append(course)
                         
-
+                    first_course = courses_list[0]
+                        
                     for i in courses_list:
                         events_list.append(getCourseEvents(i.semester, i.year, i.course_reg))
-                    
+                        
+                    defaultLogger.info('------------------END OF REQUEST-----------------')
+                        
                     return render(request,
                                   'timeedit/index.html',
-                                  {'course' : courses_list[0],
+                                  {'course' : first_course,
                                    'events' : max(events_list),
                                    'form' : form}
                               )
                 except IndexError as e:
-                    print(e)
+                    
+                    defaultLogger.info('Request failed due to IndexError Exception in IndexView.post')
+                    defaultLogger.info(e)
+                    defaultLogger.info('------------------END OF REQUEST------------------')
+
+                    errorLogger.info('Request failed due to IndexError Exception in IndexView.post')
+                    errorLogger.info('Course: %s' % course_post)
+                    errorLogger.info(e)
+                    errorLogger.info('-')
+                    
                     return render(request, 'timeedit/index.html', {'form':form, 'message':'Course does not exist, or is not active!'})
 
 
