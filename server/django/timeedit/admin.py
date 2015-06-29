dbc366d6d141d70b03467e3fb3103b38fdb01f05
John Herrlin <jherrlin@gmail.com>
2015-06-29 15:08:04 +0200

    course_location added to search fields

--- server/django/timeedit/admin.py
+++ server/django/timeedit/admin.py
@@ -5,8 +5,8 @@ from .models import Course, Event
 class CourseAdmin(admin.ModelAdmin):
     model = Course
     list_per_page = 100
-    search_fields = ['name_en', 'name_sv', 'course_code', 'course_id', 'course_reg' ,'url', 'semester', 'year']
-    list_display = ('name_en', 'name_sv', 'course_code', 'course_id', 'course_reg' ,'url', 'semester', 'year')
+    search_fields = ['name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'url', 'semester', 'year', 'course_location']
+    list_display = ('name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'url', 'semester', 'year', 'course_location')
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Event)
