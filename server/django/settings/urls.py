5df5536b8041c025d7cbea816eb95580b0555a29
John Herrlin <jherrlin@gmail.com>
2015-06-29 14:20:28 +0200

    AngularJS app added to urls

--- server/django/settings/urls.py
+++ server/django/settings/urls.py
@@ -2,12 +2,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from timeedit.views import IndexView, allCouseCodesInJSON, allCoursesInJSON, CourseView, EventView
+from timeedit.views import IndexView, CourseView, EventView
+from angular.views import IndexTemplateView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    
-    url(r'^api/only-course-codes/$', allCouseCodesInJSON, name='only-course-codes'),
+
+    url(r'^a/$', IndexTemplateView.as_view(), name='a'),
+
+    #url(r'^api/only-course-codes/$', allCouseCodesInJSON, name='only-course-codes'),
     url(r'^api/course/$', CourseView.as_view(), name='course'),
     url(r'^api/event/$', EventView.as_view(), name='event'),
     
