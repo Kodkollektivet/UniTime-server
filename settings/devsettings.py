67d8d21cae65770b82db47c99c6ad12290016165
John Herrlin <jherrlin@gmail.com>
2015-07-10 15:52:34 +0200

    requirments update settings updated to server statics from static/

--- settings/devsettings.py
+++ settings/devsettings.py
@@ -17,11 +17,6 @@ DATABASES = {
 }
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
 
 COMPRESS_ENABLED = False
 
--- settings/settings.py
+++ settings/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = 'static/'
 
 # Crispyforms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
