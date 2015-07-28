2605afc4326dcc1e6a92b35dcd3c79b4be687290
John Herrlin <jherrlin@gmail.com>
2015-07-28 14:22:10 +0200

    admin interface for course rate updated

--- courserate/admin.py
+++ courserate/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Rate
 
-admin.site.register(Rate)
+
+
+class RateAdmin(admin.ModelAdmin):
+    model = Rate
+    list_per_page = 100
+    search_fields = ['course_code', 'course_rate', 'notes']
+    list_display = ('course_code', 'course_rate', 'notes')
+
+admin.site.register(Rate, RateAdmin)
