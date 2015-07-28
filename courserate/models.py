5e1bcd8cc76f2e4a3ad42cadd376a39a5a2fd163
John Herrlin <jherrlin@gmail.com>
2015-07-28 12:11:20 +0200

    notes in course rate model have blank = true now

--- courserate/models.py
+++ courserate/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Rate(models.Model):
     course_code = models.CharField(max_length=50)
     course_rate = models.FloatField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
     ip = models.CharField(max_length=40)
 
     class Meta:
