e705806aa2920b977b93d32ff66871fee611506d
John Herrlin <jherrlin@gmail.com>
2015-07-28 16:58:52 +0200

    admin interface updated to display dates when added and modified

--- timeedit/admin.py
+++ timeedit/admin.py
@@ -5,8 +5,8 @@ from .models import Course, Event, CourseCodes
 class CourseAdmin(admin.ModelAdmin):
     model = Course
     list_per_page = 100
-    search_fields = ['name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'url', 'semester', 'year', 'course_location']
-    list_display = ('name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'url', 'semester', 'year', 'course_location')
+    search_fields = ['name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'semester', 'year', 'course_location', 'created', 'modified']
+    list_display = ('name_en', 'name_sv', 'course_code', 'course_id', 'course_reg', 'semester', 'year', 'course_location', 'created', 'modified')
 
 class CourseCodesAdmin(admin.ModelAdmin):
     model = Course
